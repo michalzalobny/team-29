915edb4a5dbb27c08793c4492640ee298494d48a
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-11-28 18:27:41 +0100

    Add reqct query for fetching and managing the data

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -5,6 +5,8 @@ import '../styles/index.scss';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import FontFaceObserver from 'fontfaceobserver';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 import { LoggerContextProvider } from 'context/LoggerContext';
 import { GlobalStyles } from 'styles/GlobalStyles';
@@ -13,6 +15,8 @@ import { Layout } from 'components/Layout/Layout';
 export default function MyApp(props: AppProps) {
   const { Component, pageProps } = props;
   const router = useRouter();
+
+  const [queryClient] = React.useState(() => new QueryClient());
 
   const [isReady, setIsReady] = useState(false);
 
@@ -36,20 +40,23 @@ export default function MyApp(props: AppProps) {
   }, []);
 
   return (
-    <LoggerContextProvider>
-      <GlobalStyles />
+    <QueryClientProvider client={queryClient}>
+      <LoggerContextProvider>
+        <GlobalStyles />
 
-      <AnimatePresence exitBeforeEnter={false}>
-        <>
-          <Layout isReady={isReady}>
-            <Component
-              key={router.route + router.locale}
-              router={router}
-              {...pageProps}
-            />
-          </Layout>
-        </>
-      </AnimatePresence>
-    </LoggerContextProvider>
+        <AnimatePresence exitBeforeEnter={false}>
+          <>
+            <Layout isReady={isReady}>
+              <Component
+                key={router.route + router.locale}
+                router={router}
+                {...pageProps}
+              />
+            </Layout>
+          </>
+        </AnimatePresence>
+      </LoggerContextProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
