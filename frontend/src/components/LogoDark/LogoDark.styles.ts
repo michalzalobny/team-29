9f54452deaf5a113d45c1e14c609afd655982585
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-11-28 19:57:38 +0100

    Add preload to logo

--- frontend/src/components/LogoDark/LogoDark.styles.ts
+++ frontend/src/components/LogoDark/LogoDark.styles.ts
@@ -1,10 +1,23 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+interface Wrapper {
+  isLoaded: boolean;
+}
 
 export const LogoImg = styled.img`
   width: 100%;
 `;
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.div<Wrapper>`
   width: 100%;
   display: flex;
+  opacity: 0;
+
+  transition: opacity 0.2s;
+
+  ${(props) =>
+    props.isLoaded &&
+    css`
+      opacity: 1;
+    `}
 `;
--- frontend/src/components/LogoDark/LogoDark.tsx
+++ frontend/src/components/LogoDark/LogoDark.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
+import { useMediaPreload } from 'hooks/useMediaPreload';
+
 import logoSrc from './img/logodark.svg';
 import * as S from './LogoDark.styles';
 
 interface Props {}
 
 export const LogoDark = (props: Props) => {
+  const { isLoaded } = useMediaPreload({ isImage: true, mediaSrc: logoSrc });
+
   return (
     <>
-      <S.Wrapper>
-        <img src={logoSrc} alt="dark logo" />
+      <S.Wrapper isLoaded={isLoaded}>
+        <S.LogoImg src={logoSrc} alt="dark logo" />
       </S.Wrapper>
     </>
   );
