22a8f63ec73ff8a7198f97d5497346e0edc124f1
lestherll <qweqwe123.aa690@gmail.com>
2022-01-07 13:45:27 +0000

    add basic Animal schema classes

--- backend/db/schemas.py
+++ backend/db/schemas.py
@@ -1,3 +1,6 @@
+from enum import Enum
+from typing import Optional
+
 from pydantic import BaseModel
 
 
@@ -16,3 +19,35 @@ class User(UserBase):
 
     class Config:
         orm_mode = True
+
+
+class Category(str, Enum):
+    """Category for an animal's population status"""
+    LEAST_CONCERN = "least_concern"
+    NEAR_THREATENED = "near_threatened"
+    VULNERABLE = "vulnerable"
+    ENDANGERED = "endangered"
+    CRITICALLY_ENDANGERED = "critically_endangered"
+    EXTINCT_IN_WILD = "extinct_in_wild"
+    EXTINCT = "extinct"
+
+
+class AnimalBase(BaseModel):
+    name: str
+    scientific_name: str
+    description: str
+    category: Category = Category.LEAST_CONCERN
+    population_low_est: Optional[int] = None
+    population_high_est: Optional[int] = None
+
+
+class AnimalCreate(AnimalBase):
+    ...
+
+
+class Animal(AnimalBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
