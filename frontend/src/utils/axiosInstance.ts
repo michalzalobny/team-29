89062dfa241d99174f95e4ddfb77070ff15b5c00
michalzalobny <michal.zalobny@lepolishbureau.com>
2022-01-11 23:26:44 +0100

    Update axios instance

--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'https://fast-api-tanimals.herokuapp.com/',
   headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
     'Cache-Control': 'no-cache',
   },
 });
