6b65e7717044182e3ad167087306a8fe7a64a783
lestherll <qweqwe123.aa690@gmail.com>
2022-01-13 00:17:19 +0000

    remove user_model_to_schema, use `dict`,method instead

--- backend/utils.py
+++ backend/utils.py
@@ -1,24 +1,4 @@
-from db import schemas, models
-
 from passlib.context import CryptContext
-
-
-def user_model_to_schema(user: models.User) -> schemas.User:
-    """
-    Convert User db model to User pydantic schema to omit password data
-    Args:
-        user: User database model
-
-    Returns:
-        User schema for safe transmission
-    """
-    return schemas.User(
-        id=user.id,
-        email=user.email,
-        username=user.username,
-        is_active=user.is_active
-    )
-
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
