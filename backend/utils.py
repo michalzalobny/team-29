584e50fece70d9ea5e01b9e5c3cbc6b5146ec72a
Sarunas Martinaitis <c0051496@newcastle.ac.uk>
2022-01-13 13:43:52 +0200

    Failed merging. Removing `user_model_to_schema` (Use `dict` method instead)

--- backend/utils.py
+++ backend/utils.py
@@ -1,24 +1,5 @@
 import logging
 from passlib.context import CryptContext
-from db import schemas, models
-
-
-def user_model_to_schema(user: models.User) -> schemas.User:
-    """
-    Convert User db model to User pydantic schema to omit password data
-    Args:
-        user: User database model
-
-    Returns:
-        User schema for safe transmission
-    """
-    return schemas.User(
-        id=user.id,
-        email=user.email,
-        username=user.username,
-        is_active=user.is_active
-    )
-
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
