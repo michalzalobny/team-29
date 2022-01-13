24954a286f28f099b16d7eda643cd00f955b2c2d
Sarunas Martinaitis <c0051496@newcastle.ac.uk>
2022-01-13 13:25:30 +0200

    Added Logger function

--- backend/utils.py
+++ backend/utils.py
@@ -1,3 +1,5 @@
+import logging
+
 from db import schemas, models
 
 from passlib.context import CryptContext
@@ -29,3 +31,19 @@ def verify_password(plain_password, hashed_password):
 
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+
+class SecurityFilter(logging.Filter):
+    def filter(self, record):
+        return "SECURITY" in record.getMessage()
+
+
+fh = logging.FileHandler('logs.log', 'a')
+fh.setLevel(logging.WARNING)
+fh.addFilter(SecurityFilter())
+formatter = logging.Formatter('%(asctime)s : %(message)s', '%m/%d/%Y %I:%M:%S %p')
+fh.setFormatter(formatter)
+
+logger = logging.getLogger('')
+logger.propagate = False
+logger.addHandler(fh)
