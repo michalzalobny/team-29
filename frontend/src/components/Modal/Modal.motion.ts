e36ac0c59983177fa637e02a71a2bfb7ce206589
michalzalobny <michal.zalobny@lepolishbureau.com>
2022-01-15 17:36:54 +0100

    Clean up modal component

--- frontend/src/components/Modal/Modal.motion.ts
+++ frontend/src/components/Modal/Modal.motion.ts
@@ -38,18 +38,3 @@ export const ModalWrapperV = {
     x: '-30%',
   },
 };
-
-export const ButtonsWrapperV = {
-  initial: {
-    opacity: 0,
-    x: '5rem',
-  },
-  animate: {
-    opacity: 1,
-    x: '0rem',
-  },
-  exit: {
-    opacity: 0,
-    x: '0rem',
-  },
-};
--- frontend/src/components/Modal/Modal.styles.ts
+++ frontend/src/components/Modal/Modal.styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
 import { media } from 'styles/media';
