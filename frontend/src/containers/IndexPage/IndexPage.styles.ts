20336e046e9fae5e9cd57db0ac602c32222c7b74
michalzalobny <michal.zalobny@lepolishbureau.com>
2022-01-16 00:25:04 +0100

    Add game component

--- frontend/src/containers/IndexPage/IndexPage.styles.ts
+++ frontend/src/containers/IndexPage/IndexPage.styles.ts
@@ -1,3 +1,19 @@
 import styled from 'styled-components';
 
+import { media } from 'styles/media';
+
 export const Wrapper = styled.div``;
+
+export const GameWrapper = styled.div`
+  padding: 0 2.5rem;
+  margin-top: 10rem;
+  display: flex;
+  align-items: center;
+
+  ${media.tablet} {
+    padding: 0;
+    width: 140rem;
+    margin: 0 auto;
+    margin-top: 10rem;
+  }
+`;
--- frontend/src/containers/IndexPage/IndexPage.tsx
+++ frontend/src/containers/IndexPage/IndexPage.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 
 import { Head } from 'seo/Head/Head';
 import { Header } from 'components/sections/Header/Header';
+import { Game } from 'components/Game/Game';
 
 import { Props } from './data';
 import * as S from './IndexPage.styles';
 
 export default function IndexPage(props: Props) {
-  const { head } = props;
+  const { animals, head } = props;
 
   return (
     <>
       <Head {...head} />
       <S.Wrapper>
         <Header />
+        <S.GameWrapper>
+          <Game animals={animals} />
+        </S.GameWrapper>
       </S.Wrapper>
     </>
   );
